6bd9a0aad3674ad4717488b6b121584664a84684
sunjung <sunjeong2201@gmail.com>
2026-01-14 16:15:09 +0900

    DMDR -13 지원자 목록 조회 페이지

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,6 +6,7 @@ import Applications from "../pages/Applications";
 import Schedule from "../pages/Schedule";
 import MyPage from "../pages/MyPage";
 import Login from "../pages/Login";
+import ApplicantList from "../pages/applicantList";
 
 export default function AppRoutes() {
   return (
@@ -17,6 +18,7 @@ export default function AppRoutes() {
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/test" element={<Test />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/applicantList" element={< ApplicantList/>} />
     </Routes>
   );
 }
